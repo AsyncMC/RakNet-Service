db8b982b84256709f5bc3dc837e9cf36e38695fd
joserobjr <joserobjr@gamemods.com.br>
2021-01-06 11:30:52 -0300

    Initial commit

---
+++ src/test/kotlin/com/github/asyncmc/template/internal/Dummy2Test.kt
@@ -0,0 +1,11 @@
+package com.github.asyncmc.template.internal
+
+import org.junit.jupiter.api.Test
+
+internal class Dummy2Test {
+    @Test
+    fun dummyTest() {
+        val dummy = Dummy2()
+        dummy.codecovNeedsMoreFiles(areYouWorkingNow = "i don't know")
+    }
+}
---
+++ src/test/kotlin/com/github/asyncmc/template/internal/DummyTest.kt
@@ -0,0 +1,11 @@
+package com.github.asyncmc.template.internal
+
+import org.junit.jupiter.api.Test
+
+internal class DummyTest {
+    @Test
+    fun dummyTest() {
+        val dummy = Dummy()
+        dummy.codecov("i don't know")
+    }
+}
